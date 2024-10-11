d702499069e9062ac3690ebff082fdf8f4f6e177
Adjibousso <diallobousso10@gmail.com>
2024-10-11 14:28:11 -0400

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors =require('cors')
+require("dotenv").config()
+// const { default: mongoose } = require('mongoose')
+const mongoose =require('mongoose')
+
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.post('/api/register',(req,res)=> {
+
+    console.log(req.body)
+    res.json({status:'ok'})
+})
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World! how are you?')
 })
-// username:diallobousso10
-// password : nLZO30myIf0BY7Qx
 
-mongodb+srv://diallobousso10:<nLZO30myIf0BY7Qx>@backend-caps.xm1ai.mongodb.net/?retryWrites=true&w=majority&appName=Backend-caps
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
+  mongoose.connect(process.env.MONGO_URL)
+  .then(()=>{
+    console.log("databse conntected")
+  })
 })
