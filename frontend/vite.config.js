b45e1f58d4e130a1b00de7b40e492e6816e5e72f
Adjibousso <diallobousso10@gmail.com>
2024-10-17 01:29:32 -0400

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1600, // Adjust this value based on your needs
+  },
   plugins: [react()],
   server:{
     proxy:{
