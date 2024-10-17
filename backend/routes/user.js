ce0a8849175f3b77b0753955eb168387b8efc3c0
Adjibousso <diallobousso10@gmail.com>
2024-10-17 09:26:05 -0400

    final touch

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,19 +3,19 @@ const { createUser, getUsers, getUserById, updateUser, deleteUser } = require('.
 
 const router = express.Router();
 
-// Create a new user
+// create a new user
 router.post('/register', createUser);
 
-// Get all users
+// get all users
 router.get('/', getUsers);
 
-// Get a single user by ID
+//  a  user by ID
 router.get('/:id', getUserById);
 
-// Update a user by ID
+// update a user by ID
 router.put('/:id', updateUser);
 
-// Delete a user by ID
+// dlete a user by ID
 router.delete('/:id', deleteUser);
 
 module.exports = router;
